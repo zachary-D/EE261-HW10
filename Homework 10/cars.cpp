34abeb05363428558017ac837723f4685fd01c82
Unknown <zacharyd1@live.com>
2017-11-06 13:41:48 -0500

    In progress

--- Homework 10/cars.cpp
+++ Homework 10/cars.cpp
@@ -9,6 +9,14 @@ using namespace std;
 
 struct Date
 {
+	Date::Date() {}
+	Date::Date(int _day, int _month, int _year)
+	{
+		day = _day;
+		month = _month;
+		year = _year;
+	}
+
 	int month;
 	int day;
 	int year;
@@ -16,9 +24,29 @@ struct Date
 
 struct Car
 {
+	Car::Car() {}
+
+	Car::Car(string _customer, float _price, Date _purchased)
+	{
+		price = _price;
+		purchased = _purchased;
+		customer = _customer;
+		sold = false;
+	}
+
+	Car::Car(string _customer, float _price, Date _purchased, Date _soldDate)
+	{
+		(*this) = Car(_customer, _price, _purchased);
+		sold = true;
+		soldDate = _soldDate;
+	}
+
 	float price;
 	Date purchased;
 	string customer;
+
+	bool sold;	//If a car was sold
+	Date soldDate;	//The date the car was sold (undefined if not sold)
 };
 
 // Pre:  File dataIn has been opened.
@@ -31,6 +59,13 @@ Car GetCar(ifstream&  dataIn)
 		>> car.purchased.month >> car.purchased.year;
 	dataIn.ignore(2, '\n');
 	return car;
+}
+
+Car CarSold(Date _date, Car _car)
+{
+	_car.sold = true;
+	_car.soldDate = _date;
+	return _car;
 }
 
 // Pre:  File dataOut has been opened.
@@ -59,6 +94,20 @@ int main()
 	while (dataIn)
 	{
 		car.price = car.price * 1.10;
+
+		cout << "The car's owner is " << car.customer << endl;
+		cout << "Has the car been resold? <y/n>" << endl;
+
+		char response;
+		while (true)
+		{
+			cout << ">";
+			response = cin.get();
+			if (tolower(response) == 'y' && tolower(response) == 'n') break;
+			cout << "I'm sorry, I don't understand \"" << response << "\".  Please try again." << endl;
+		}
+		if(response == 'y' 
+
 		WriteCar(dataOut, car);
 		car = GetCar(dataIn);
 	}
